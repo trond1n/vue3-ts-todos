4ad9af30c21ce5049c12bd6bb272b6457d038b50
Yuri Trondin <manctzns@gmail.com>
2023-08-07 22:29:04 +0500

    done

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <AppHeader />
-  <AppFilters />
+  <AppFilters :active-filter="activeFilter" @set-filter="seTfilter" />
 
   <main class="app-main">
     <AppTodoList
-      :todos="todos"
+      :todos="filteredTodos"
       @toggle-todo="toggleTodo"
       @remove-todo="removeTodo"
     />
     <AppAddTodo @add-todo="addTodo" />
   </main>
-  <AppFooter />
+  <AppFooter :stats="stats" />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -18,10 +18,12 @@ import AppHeader from "./components/AppHeader.vue";
 import AppFilters from "./components/AppFilters.vue";
 import AppTodoList from "./components/AppTodoList.vue";
 import AppAddTodo from "./components/AppAddTodo.vue";
-import AppFooter from "./components/AppFooter.vue";
+import AppFooter, { Stats } from "./components/AppFooter.vue";
 import { Todo } from "./types/Todo";
+import { Filter } from "./types/Filter";
 interface State {
   todos: Todo[];
+  activeFilter: Filter;
 }
 export default defineComponent({
   components: {
@@ -38,7 +40,33 @@ export default defineComponent({
         { id: 1, text: "Learn the basics of Typescript", completed: false },
         { id: 2, text: "Learn the basics of Vue", completed: false },
       ],
+      activeFilter: "All",
     };
+  },
+  computed: {
+    filteredTodos(): Todo[] {
+      switch (this.activeFilter) {
+        case "Active":
+          return this.activeTodos;
+        case "Done":
+          return this.doneTodos;
+        case "All":
+        default:
+          return this.todos;
+      }
+    },
+    stats(): Stats {
+      return {
+        active: this.activeTodos.length,
+        done: this.doneTodos.length,
+      };
+    },
+    activeTodos(): Todo[] {
+      return this.todos.filter((todo) => !todo.completed);
+    },
+    doneTodos(): Todo[] {
+      return this.todos.filter((todo) => todo.completed);
+    },
   },
   methods: {
     toggleTodo(id: number) {
@@ -52,6 +80,9 @@ export default defineComponent({
     },
     addTodo(todo: Todo) {
       this.todos.push(todo);
+    },
+    seTfilter(filter: Filter) {
+      this.activeFilter = filter;
     },
   },
 });
